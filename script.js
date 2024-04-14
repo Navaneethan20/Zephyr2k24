e41266734a2da688461e691daf78a593e6585516
Navaneethan20 <thulasinavaneethan22@gmail.com>
2024-04-14 08:27:02 +0530

    " Zephyr website "

---
+++ script.js
@@ -0,0 +1,41 @@
+var countDownDate = new Date("April 25, 2024 00:00:00").getTime();
+
+// Update the countdown every 1 second
+var x = setInterval(function() {
+
+  // Get the current date and time
+  var now = new Date().getTime();
+
+  // Calculate the remaining time
+  var distance = countDownDate - now;
+
+  // Calculate days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the countdown
+  document.getElementById("days").innerHTML = days;
+  document.getElementById("hours").innerHTML = hours;
+  document.getElementById("minutes").innerHTML = minutes;
+  document.getElementById("seconds").innerHTML = seconds;
+
+  // If the countdown is over, display text
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("countdown").innerHTML = "EXPIRED";
+  }
+}, 1000);
+
+
+// Get the necessary elements
+const menuToggle = document.querySelector('.menu-toggle');
+const navLinks = document.querySelector('.nav-links');
+
+// Add an event listener to the toggle button
+menuToggle.addEventListener('click', function() {
+  // Toggle the 'show' class on the navigation links container and the toggle button
+  navLinks.classList.toggle('show');
+  menuToggle.classList.toggle('show');
+});
